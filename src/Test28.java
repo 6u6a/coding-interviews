06171dc9cd37d6fe3ebd58c6476a06d419ba6266
6u6a <15762254497@139.com>
2018-07-31 15:28:35 +0800

    Test28 bug fixed and enhancement!

--- src/Test28.java
+++ src/Test28.java
@@ -1,56 +1,59 @@
 /**
- * Author: 王俊超
- * Date: 2015-05-06
- * Time: 08:20
- * Declaration: All Rights Reserved !!!
+ * Author: rogers
+ * Date: 2018-07-31
+ * Time: Afternoon
+ * Declaration: opensource！！！
  */
 public class Test28 {
     /**
      * 题目：输入一个字符串，打印出该字符事中字符的所有排列。例如输入字符串abc。
      * 则打印出由字符a、b、c 所能排列出来的所有字符串abc、acb、bac、bca、cab和cba。
-     *
-     * @param chars 待排序的字符数组
+	 * 备注：该版本可以支持含有相同字符的字符串，比如：aabb
      */
-    public static void permutation(char[] chars) {
-        // 输入较验
-        if (chars == null || chars.length < 1) {
-            return;
-        }
-        // 进行排列操作
-        permutation(chars, 0);
-    }
-
+	public static int cha[];
+	
     /**
      * 求字符数组的排列
      *
-     * @param chars 待排列的字符串
+     * @param res 结果字符串
      * @param begin 当前处理的位置
+	 * @param total 待排序字符串的总长度
      */
-    public static void permutation(char[] chars, int begin) {
-        // 如果是最后一个元素了，就输出排列结果
-        if (chars.length - 1 == begin) {
-            System.out.print(new String(chars) + " ");
+    public static void permutation(char[] res, int begin, int total) {
+        if (total == begin) {
+            System.out.print(new String(res) + " ");
         } else {
-            char tmp;
-            // 对当前还未处理的字符串进行处理，每个字符都可以作为当前处理位置的元素
-            for (int i = begin; i < chars.length; i++) {
-                // 下面是交换元素的位置
-                tmp = chars[begin];
-                chars[begin] = chars[i];
-                chars[i] = tmp;
-
-                // 处理下一个位置
-                permutation(chars, begin + 1);
+            for (int i = 0; i < cha.length; i ++) {
+                if(cha[i] > 0){
+                    cha[i] --;
+                    res[begin] = (char)('a' + i);
+                    permutation(res, begin + 1, total);
+                    cha[i] ++;
+                }
             }
         }
     }
 
     public static void main(String[] args) {
         char[] c1 = {'a', 'b', 'c'};
-        permutation(c1);
+        char res[] = new char[c1.length];
+        int i;
+        cha = new int[26];
+        //统计数组中每个字符的个数
+		for(i = 0; i < c1.length; i ++){
+            cha[c1[i] - 'a'] ++;
+        }
+        permutation(res, 0, c1.length);
         System.out.println();
 
-        char[] c2 = {'a', 'b', 'c', 'd'};
-        permutation(c2);
+        char[] c2 = {'a', 'a', 'b', 'b'};
+        res = new char[c2.length];
+        cha = new int[26];
+		//统计数组中每个字符的个数
+        for(i = 0; i < c2.length; i ++){
+            cha[c2[i] - 'a'] ++;
+        }
+        permutation(res, 0, c2.length);
+        System.out.println();
     }
 }
